10ed79f73afd36f70630e4bbec13ee43eff1738b
Ahmed Reda <92978953+ahmedredaooooo@users.noreply.github.com>
2023-10-14 00:56:19 +0300

    edit File

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * _isdigit -
- * @c:
- * Return:
+ * _isdigit - 5555 5 5 
+ * @c: 454 54 54 5
+ * Return:464 64 4654
  */
 int _isdigit(int c)
 {
 if (c >= '0' && c <= '9')
+{
     return (1);
+}
 else
+{
     return (0);
+}
 }
