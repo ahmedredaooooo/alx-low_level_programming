229090596d9f53fa162bcb03e00d36f692cb9f5d
Ahmed Reda <92978953+ahmedredaooooo@users.noreply.github.com>
2023-10-17 01:33:32 +0300

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int length = 0;
+int i;
+/* get the length of the string */
+while (s[length] != '\0')
+{
+length++;
+}
+/* print the string in reverse */
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
