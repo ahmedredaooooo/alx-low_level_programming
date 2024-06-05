cf95c1fd49b99b2849c6be80bfc5171d3be48a46
ahmedredaooooo <ahmedredaqadry@gmail.com>
2024-06-05 02:23:01 -0700

    code edited

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,7 +16,7 @@ def island_perimeter(grid):
                 ret += 1
                 if j >= 1 and grid[i][j - 1] == 1 or i >= 1 and grid[i - 1][j] == 1:
                     edges += 1
-    return ((ret << 2) - (edges << 1))
+    return ((ret * 4) - (edges * 2))
 
 
 
