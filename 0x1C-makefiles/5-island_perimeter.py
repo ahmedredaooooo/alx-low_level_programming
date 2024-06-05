700f79c37428c5d6b70a865117f097ccad7fa54e
ahmedredaooooo <ahmedredaqadry@gmail.com>
2024-06-05 02:14:16 -0700

    Island Perimeter SOLVED

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+"""
+
+def island_perimeter(grid):
+    """ returns the perimeter of island in grid """
+    n = len(grid)
+    m = len(grid[0])
+    ret = 0
+    edges = 0
+
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1:
+                ret += 1
+                if j >= 1 and grid[i][j - 1] == 1 or i >= 1 and grid[i - 1][j] == 1:
+                    edges += 1
+    return ((ret << 2) - (edges << 1))
+
+
+
+
